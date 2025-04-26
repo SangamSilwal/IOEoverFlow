c89e8034ebb6359c3b3c0e9824add778b741e387
SangamSilwal <sangamsilwal2062@gmail.com>
2025-04-26 11:11:20 +0545

    Added content Schema

---
+++ backend/src/models/content.model.js
@@ -0,0 +1,39 @@
+import mongoose,{ Schema} from "mongoose";
+
+const contentSchema = new Schema({
+    title:{
+        type: String
+    },
+    description:{
+        type:String
+    },
+    fileAttach:{
+        type:String
+    },
+    author:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    likes:[
+        {
+            user:{type:Schema.Types.ObjectId,ref:"User"},
+            createdAt:{type:Date,default:Date.now}
+        }
+    ],
+    comments:[
+        {
+            user:{type:Schema.Types.ObjectId,ref:"User"},
+            text:String,
+            replies:[
+                {
+                    user:{type:Schema.Types.ObjectId,ref:"User"},
+                    text:String,
+                    createdAt:{type:Date,default:Date.now} 
+                }
+            ],
+            createdAt:{type:Date,default:Date.now},
+        }
+    ]
+})
+
+export const Content = mongoose.model("Content",contentSchema)
