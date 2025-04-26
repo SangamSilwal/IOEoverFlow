7b0915e3073b479ec8ac9a7f6f14a58de938a759
SangamSilwal <sangamsilwal2062@gmail.com>
2025-04-26 09:46:38 +0545

    User Model Prepared

---
+++ backend/src/constants.js
@@ -0,0 +1,40 @@
+const IOEColleges = {
+    PULCHOWK_CAMPUS: "Pulchowk Campus (IOE Central Campus)",
+    THAPATHALI_CAMPUS: "Thapathali Campus",
+    PASCHIMANCHAL_CAMPUS: "Paschimanchal Campus (Pokhara)",
+    PURWANCHAL_CAMPUS: "Purwanchal Campus (Dharan)",
+    KEC: "Kathmandu Engineering College (KEC)",
+    ACEM: "Advanced College of Engineering and Management (ACEM)",
+    NEC: "Nepal Engineering College (NEC)",
+    HCOE: "Himalaya College of Engineering (HCOE)",
+    KHWOPA_COE: "Khwopa College of Engineering (Khwopa COE)",
+    NCE: "National College of Engineering (NCE)",
+    SEC: "Sagarmatha Engineering College (SEC)",
+    LEC: "Lalitpur Engineering College (LEC)",
+    ACME: "Acme Engineering College",
+    CCMT: "Cosmos College of Management and Technology (CCMT)",
+    KATHFORD: "Kathford International College of Engineering and Management",
+    KANTIPUR_COE: "Kantipur Engineering College (Kantipur COE)",
+    NCIT: "Nepal College of Information Technology (NCIT)",
+    OCEM: "Oxford College of Engineering and Management (OCEM)",
+};
+const ioeBachelorPrograms = {
+    BCE: "Bachelor in Civil Engineering (B.E. Civil)",
+    BEL: "Bachelor in Electrical Engineering (B.E. Electrical)",
+    BEI: "Bachelor in Electronics & Communication Engineering (B.E. Electronics & Comm.)",
+    BCT: "Bachelor in Computer Engineering (B.E. Computer)",
+    BME: "Bachelor in Mechanical Engineering (B.E. Mechanical)",
+    BGE: "Bachelor in Geomatics Engineering (B.E. Geomatics)",
+    BArch: "Bachelor in Architecture (B.Arch)",
+    BAE:"Bachelor of Aerospace Engineering Course, BAE",
+    BEC:"Bachelor in Chemical Engineering"
+  };
+
+
+const ioeBachelorProgramsValues = Object.values(ioeBachelorPrograms)
+const ioeCollegeValues = Object.values(IOEColleges);
+
+export {
+    ioeBachelorProgramsValues,
+    ioeCollegeValues
+}
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,122 @@
+import mongoose,{Schema,model} from "mongoose";
+import { ioeCollegeValues,ioeBachelorProgramsValues} from "../constants.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim:true,
+        maxlength: 40
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        maxlength:20,
+        index:true,
+        lowercase:true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true
+    },
+    password: {
+        type: String,
+        required: [true,"Password is required"]
+
+    },
+    biostatus: {
+        type: String,
+    },
+    collegeName: {
+        type: String,
+        required: true,
+        enum : {
+            values: ioeCollegeValues,
+            message: `{value} is not a valid IOE-affiliated college name. Supported colleges are ${ioeCollegeValues.join(', ')}`,
+        }
+    },
+    year: {
+        type: Number,
+    },
+    departMent: {
+        type: String,
+        required: true,
+        enum: {
+            values: ioeBachelorProgramsValues,
+            message: `{value} is not a valid program in IOE`
+        },
+
+    },
+    refreshToken:{
+        type:String
+    },
+    profilePicUrl: {
+        type: String,
+        default:""
+    },
+    content: [{
+        type: Schema.Types.ObjectId,
+        ref:'Content'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    }],
+    friendRequest: [{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    }],
+    sentRequest: [{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    }]
+
+},{timestamps: true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password)
+{
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.genrateAccessToken = async function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            name:this.name,
+            username:this.username,
+            email:this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.genrateRefreshToken = async function()
+{
+    return jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
+export const User = mongoose.model("User",userSchema)
